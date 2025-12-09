e9a74acc250110af22e74aeb6dc4492f3313fba5
Ysato <y.sugar0402@gmail.com>
2025-12-10 00:38:24 +0900

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './',
+      build: {
+        outDir: 'docs',
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
